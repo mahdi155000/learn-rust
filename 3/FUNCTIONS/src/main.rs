e8d5a30b5fd1ba7a25b6b8464d7ad8c5ab1714ab
mahdi <mahdi155000@gmail.com>
2024-07-21 18:29:35 +0330

    add new type of runction and two diffrent usage.

--- 3/FUNCTIONS/src/main.rs
+++ 3/FUNCTIONS/src/main.rs
@@ -2,10 +2,21 @@ fn main() {
     println!("Hello, world!");
 
 
-    test_function();
+    test_function(16);
+
+    let x = five();
+    println!("The value of five is: '{}'", x);
+    println!("The value of function five is: '{}'", five());
 }
 
-fn test_function()
+fn test_function(x: i16)
 {
-    println!("This is test function return");
+    println!("The value of x is: '{}'", x);
 }
+
+fn five() -> i16
+{
+    5
+}
+
+    
