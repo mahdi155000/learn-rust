3543720ca459a373c6db9f404ada5a20fc669d2c
mahdi <mahdi155000@gmail.com>
2024-07-22 00:14:55 +0330

    add commnet for condition condition.

---
+++ 3/branches/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+
+    let number = 6;
+    //The condition is this code must be 'bool' otherwise we'll get an error.
+    if number < 5
+    {
+        println!("condition was true.");
+    } else {
+        println!("condition was false.");
+    }
+}
