84f90a5d95aff79101ea4df0f32b693d9cb250da
mahdi <mahdi155000@gmail.com>
2024-07-22 02:53:29 +0330

    add else if into program

--- 3/branches/src/main.rs
+++ 3/branches/src/main.rs
@@ -3,10 +3,22 @@ fn main() {
 
     let number = 6;
     //The condition is this code must be 'bool' otherwise we'll get an error.
-    if number < 5
+    if number != 5
     {
         println!("condition was true.");
     } else {
         println!("condition was false.");
     }
+
+    if number % 4 == 0
+    {
+        println!("Number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("Number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("Number is divisible by 2");
+    } else {
+        println!("Number is not divisible by 4, 3 or 2");
+    }
 }
+
