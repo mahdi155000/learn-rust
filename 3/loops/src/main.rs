b11858365a83bad9f6e0181c7546b9bc906187b5
mahdi <mahdi155000@gmail.com>
2024-07-22 03:37:11 +0330

    end loop with deffining a variable

--- 3/loops/src/main.rs
+++ 3/loops/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
+    // loop loop
+    let mut loop_break = 0 ;
     loop {
         println!("again!");
+        loop_break += 1;
+        if loop_break >= 5 {
+            println!("end of the loop.");
+            break;
+        }
+        
     }
 }
