2d821352e61f997400fdb76e23def992ac032eaf
mahdi <mahdi155000@gmail.com>
2024-07-22 03:39:54 +0330

    add while loop into main.rs

--- 3/loops/src/main.rs
+++ 3/loops/src/main.rs
@@ -5,9 +5,15 @@ fn main() {
         println!("again!");
         loop_break += 1;
         if loop_break >= 5 {
-            println!("end of the loop.");
+            println!("end of the loop.\n");
             break;
         }
-        
+    }
+
+    let mut while_break = 10;
+
+    while while_break != 0 {
+        println!("{}!", while_break);
+        while_break -= 1;
     }
 }
