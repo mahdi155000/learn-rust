a84c1af2d9dcaab91455193d413742cadb9d894c
mahdi <mahdi155000@gmail.com>
2024-07-23 11:57:12 +0330

    fix error when write a text exchange of number

--- 2/guessing_game/src/main.rs
+++ 2/guessing_game/src/main.rs
@@ -22,14 +22,16 @@ fn main()
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {println!("you have to enter number!!"); continue;},
+    };
 
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {println!("You win!"); break;},
         Ordering::Greater => println!("Too big"),
     }
     }
