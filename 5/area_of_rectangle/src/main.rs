699ac3e283911519afff14f80f72d95f9c00a778
mahdi <mahdi155000@gmail.com>
2025-02-17 16:19:55 +0330

    make the result better by print the result using {:?} debug format.

--- 5/area_of_rectangle/src/main.rs
+++ 5/area_of_rectangle/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     length: u32,
@@ -5,10 +6,11 @@ struct Rectangle {
 
 fn main() {
     let rect1 = Rectangle { width: 30, length: 50};
-    println!(
+/*    println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1));
-
+*/
+    println!("rect1 is {:?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
