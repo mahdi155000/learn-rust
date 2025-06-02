e23292c11f87fd989d7ce7176781afe7c059abee
mahdi <mahdi155000@gmail.com>
2025-06-02 16:00:19 +0330

    make the function support multi type

--- 10/function/src/main.rs
+++ 10/function/src/main.rs
@@ -1,20 +1,20 @@
-fn largest(list: &[i32]) -> i32 {
-	let mut largest = list[0];
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
 	for &item in list.iter() {
 		if item > largest {
 			largest = item;
 		}
 	}
 	largest
+
 }
 fn main() {
-	let number_list = vec![34, 50, 25,100,65];
-	let result = largest(&number_list);
-	println!("The largest number is {}", result);
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-	let number_list = vec![100, 300, 00, 6000, 39, 15];
-	let result = largest(&number_list);
-	println!("The largest number is {}", result);
-
-	
+    let char_list= vec!['y', 'm', 'a', 'q', 'z'];
+    let result = largest(&char_list);
+    println!("The largest number is {}", result);
 }
