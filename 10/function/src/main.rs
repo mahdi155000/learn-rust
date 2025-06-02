2963f145df596a5d4650e613fea36679929145d9
mahdi <mahdi155000@gmail.com>
2025-06-02 15:43:03 +0330

    write a funciton to prevent repeat the code

--- 10/function/src/main.rs
+++ 10/function/src/main.rs
@@ -1,24 +1,20 @@
+fn largest(list: &[i32]) -> i32 {
+	let mut largest = list[0];
+	for &item in list.iter() {
+		if item > largest {
+			largest = item;
+		}
+	}
+	largest
+}
 fn main() {
 	let number_list = vec![34, 50, 25,100,65];
-	
-	let mut largest = number_list[0];
+	let result = largest(&number_list);
+	println!("The largest number is {}", result);
 
-	for number in number_list {
-		if number > largest {
-			largest = number
-			}
-	}
+	let number_list = vec![100, 300, 00, 6000, 39, 15];
+	let result = largest(&number_list);
+	println!("The largest number is {}", result);
 
-	println!("The largest number is {}", largest);
-
-	let number_list = vec![102, 34, 6000, 89, 54, 2, 42, 8];
-
-	let mut largest = number_list[0];
-	for number in number_list {
-		if number > largest {
-			largest = number;
-			}
-		}
-	println!("The largest number is {}", largest);
 	
 }
