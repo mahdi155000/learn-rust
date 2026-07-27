6dd02dc36c751b59399d4966afcf8d78e09692c3
mahdi155000 <mahdi155000@gmail.com>
2026-07-27 12:27:40 +0330

    loop and while loops

---
+++ 3/loops/src/main.rs
@@ -0,0 +1,13 @@
+fn main(){
+    loop {
+        println!("again");
+        break;
+    }
+
+    let mut number = 3;
+     while number != 0 {
+        println!("{}!", number);
+
+        number = number -1;
+     }
+}
