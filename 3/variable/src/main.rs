21e439cafc13d005ad552fdab882010ec8960795
mahdi155000 <mahdi155000@gmail.com>
2026-07-27 12:21:39 +0330

    conditions ( if, else if, else)...

--- 3/variable/src/main.rs
+++ 3/variable/src/main.rs
@@ -6,4 +6,21 @@ fn main(){
         x + 1
     };
     println!("The value of y is: {}", y);
+    let z = five();
+    println!("The value of z is: {}", z);
+
+    let condition = true;
+    let numbers = if condition {
+        5
+    } else {
+        6
+    };
+
+    println!("The value of numbers is: {}", numbers);
+}
+
+
+
+fn five() -> i32 {
+    5
 }
