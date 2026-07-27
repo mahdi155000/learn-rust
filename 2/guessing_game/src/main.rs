f69ab847cdbd68548df08b4d61a590bd867a2384
mahdi155000 <mahdi155000@gmail.com>
2026-07-27 12:11:17 +0330

    write an example of changing the value of a variable with let

--- 2/guessing_game/src/main.rs
+++ 2/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+use core::num;
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
@@ -10,21 +11,30 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big")
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+            Ordering::Greater => println!("Too big")
+        }
+        
     }
+
 }
