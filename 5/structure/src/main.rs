fce6d4a8d7e208fd6f36b5fad2bbac50d694e0c2
mahdi155000 <mahdi155000@gmail.com>
2026-07-29 13:17:33 +0330

    make code look better

--- 5/structure/src/main.rs
+++ 5/structure/src/main.rs
@@ -1,14 +1,25 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+
 fn main() {
-    let rectangle = (30, 50);
+    // let rectangle = (30, 50);
+    let rect1 = Rectangle{height: 50, width: 30};
 
     println!("The area of the rectangle is {} sqaure pixels.",
-            area(rectangle));
+            area(&rect1));
    
 }
 
 
-fn area(domainsions: (u32, u32)) -> u32 {
-    domainsions.0 * domainsions.1
+// fn area(domainsions: (u32, u32)) -> u32 {
+//     domainsions.0 * domainsions.1
+// }
+
+fn area(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
 }
 
 fn build_user(email:String, username: String) -> User{
