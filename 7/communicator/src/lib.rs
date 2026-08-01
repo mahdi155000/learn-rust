83de68416a002729c2cb590317c5f03940c3cc7a
mahdi155000 <mahdi155000@gmail.com>
2026-08-01 13:19:59 +0330

    writing my first module

---
+++ 7/communicator/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod network{
+    fn connect() {
+
+    }
+}
+
+mod client {
+    fn connect() {
+
+    }
+}
