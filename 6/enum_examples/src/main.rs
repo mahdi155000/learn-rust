da0e6325d312ed7bfd8f2e68a2820029ee63970e
mahdi155000 <mahdi155000@gmail.com>
2026-08-01 09:45:47 +0330

    wriing an example of useing enum

--- 6/enum_examples/src/main.rs
+++ 6/enum_examples/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 }
+
+
+
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+let home = IpAddr {
+    kind: IpAddrKind::v4,
+    address: String::from("127.0.0.1"),
+};
+
+let loopback = IpAddr {
+    kind: IpAddrKind::V6,
+    address: String::from("::1"),
+};
