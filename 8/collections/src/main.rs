a3f4d02c6b0373ea85e1da748e12ab02e15e7a9b
mahdi155000 <mahdi155000@gmail.com>
2026-08-05 22:37:32 +0330

    accessing to the vector item with for loop

--- 8/collections/src/main.rs
+++ 8/collections/src/main.rs
@@ -9,8 +9,11 @@ fn main() {
     let v2 = vec![1,2,3,4];
     println!("{:?}", v2);
 
-    let third_in_v: &i32 = &v[9]; // break the program
+    // let third_in_v: &i3 = &v[9]; // break the program
     let third2_in_v: Option<&i32> = v.get(9); // return none
     
+    for i in &v {
+        println!("{i}");
+    }
 
 }
