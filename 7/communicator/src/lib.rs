7ff0ef6ebe9c7b5baf1bed7edbb2133ba194c6bf
mahdi155000 <mahdi155000@gmail.com>
2026-08-05 12:32:05 +0330

    write an example of vectors and try to access to it's indexes

--- 7/communicator/src/lib.rs
+++ 7/communicator/src/lib.rs
@@ -8,8 +8,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        client::connect();
     }
 }
 
---
+++ 8/collections/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // Vector
+    let mut v: Vec<i32> = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(8);
+    v.push(7);
+    println!("{:?}", v);
+    let v2 = vec![1,2,3,4];
+    println!("{:?}", v2);
+
+    let third_in_v: &i32 = &v[9]; // break the program
+    let third2_in_v: Option<&i32> = v.get(9); // return none
+    
+
+}
